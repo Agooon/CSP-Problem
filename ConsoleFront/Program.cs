174241ca52062888a8270b12ed02e995c6d1de0e
Agooon <agonplp@gmail.com>
2021-03-29 10:58:44 +0200

    Add project files.

---
+++ ConsoleFront/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleFront
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
